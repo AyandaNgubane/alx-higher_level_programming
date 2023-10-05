e9c24606c298cd02711b26a319459671f0b4a84c
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-05 04:42:46 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     h_names = dir(hidden_4)
 
     for i in range(len(h_names)):
-        if (h_names[i][0:2] != "__":
+        if (h_names[i][0:2] != "__"):
                 print(h_names[i])
