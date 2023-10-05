f73e6c37404d86a4bd91e34a2d69c8e390b6aada
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-05 03:49:44 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
 
     args = len(sys.argv) - 1
 
-    if (len(sys.argv) > 2 or len(sys.argv) == 1):
+    if (len(sys.argv) > 2):
         print(args, "arguments:")
+    elif (len(sys.argv) == 1):
+        print(args, "arguments.")
     else:
         print(args, "argument:")
-    for i in range(len(sys.argv)):
-        if ( i != 0):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    for i in range(1, len( sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
