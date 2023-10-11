2475437e8fc464353f5ba675d305eeaddd1f197d
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-11 07:06:03 -0700

    Task 0 and Task 10

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    sqrd = list(map(lambda row: list(map(lambda x: x * x, row)), new_matrix))
+    #new_matrix = matrix.copy()
+    sqrd = list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
     return (sqrd)
