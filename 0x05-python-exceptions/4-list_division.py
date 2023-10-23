a18d8fa2bd13c49f43b3d1fbb657c54e27e79f67
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-23 05:23:25 -0700

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,10 +8,13 @@ def list_division(my_list_1, my_list_2, list_length):
             quotient = my_list_1[i] / my_list_2[i]
         except IndexError:
             print("out of range")
+            quotient = 0
         except TypeError:
             print("wrong type")
+            quotient = 0
         except ZeroDivisionError:
             print("division by 0")
+            quotient = 0
         finally:
             my_list.append(quotient)
     return (my_list)
