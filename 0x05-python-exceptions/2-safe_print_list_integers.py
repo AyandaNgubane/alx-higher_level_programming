2e2069d47eed7099ace9fe4327ad4ce28fad0574
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-23 04:54:38 -0700

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    valid = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            valid += 1
+        except IndexError:
+            break
+        except TypeError:
+            continue
+    print()
+    return (valid)
