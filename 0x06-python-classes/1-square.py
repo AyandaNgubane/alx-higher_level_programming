36490a506251486b75540b42fd2ef0f70708526b
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-24 05:07:13 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size):
+        """attributes
+
+        args:
+            size - size of square
+            """
+
+        self.__size = size
