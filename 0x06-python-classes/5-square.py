1b6d20edc9a671c86dae071cdc260bf6a17c87f7
Ayanda Ngubane <ayanda4me@gmail.com>
2023-10-24 05:55:46 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """attributes
+
+        args:
+            size - size of square
+            """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """make size private
+
+        args: none
+
+        Return: private size
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size
+
+        args:
+            value - value
+        Return: none
+        """
+
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area of square
+
+        args: none
+        Returns: area
+        """
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints square using #
+
+        args: none
+        Return: none
+        """
+
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
+        else:
+            print()
