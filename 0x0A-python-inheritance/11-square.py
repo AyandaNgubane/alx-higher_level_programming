8755dac4b17d68f8e4b9f6a30e6f8f40dfa39402
Ayanda Ngubane <ayanda4me@gmail.com>
2023-11-06 05:19:05 -0800

    Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,23 +4,35 @@
 class Square that inherits from Rectangle (9-rectangle.py).
 (task based on 10-square.py)
 """
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(Rectangle):
+class Square(BaseGeometry):
     """
-    Represents a square
+    A Square class shape, inheirts from BaseGeometry
     """
-
     def __init__(self, size):
-        """initialise"""
+        """"
+        Init function for Square
 
-        self.integer_validator(size, size)
-        super().__init__(size, size)
+        Attributes:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
         self.__size = size
 
     def __str__(self):
-        """return"""
+        """
+        str funtion to print with/height
 
+        Returns:
+            Return width/height
+        """
         return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """
+        A function that calculates the area of the Square
+        """
+        return self.__size ** 2
