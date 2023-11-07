639c9717d50c1f5840f54759da65b70df48f0106
Ayanda Ngubane <ayanda4me@gmail.com>
2023-11-07 02:25:50 -0800

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,6 +15,7 @@ def print_stats(size, status_codes):
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
+
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
