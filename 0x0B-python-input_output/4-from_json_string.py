8acd3dbd90159066cc81fae3138f05dd4f2c9074
Ayanda Ngubane <ayanda4me@gmail.com>
2023-11-07 02:14:47 -0800

    pycodestyle

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,8 @@ Write a function that returns an object (Python data structure)
 represented by a JSON string:
 
 Prototype: def from_json_string(my_str):
-You don’t need to manage exceptions if the JSON string doesn’t represent an object.
+You don’t need to manage exceptions if the JSON string
+doesn’t represent an object.
 """
 
 import json
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a function that writes an Object to a text file, using a JSON representation:
+Write a function that writes an Object to a text file,
+using a JSON representation:
 
 Prototype: def save_to_json_file(my_obj, filename):
 You must use the with statement
@@ -21,5 +22,5 @@ def save_to_json_file(my_obj, filename):
         filename - file name
     """
 
-     with open(filename, "w") as f:
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
