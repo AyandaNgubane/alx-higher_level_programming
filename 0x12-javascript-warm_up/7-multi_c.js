5c78081f2f4cdaba7d7d84702dfaece329f84eb1
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-08 02:18:36 -0800

    semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,13 +2,12 @@
 
 const x = parseInt(process.argv[2]);
 
-if (x){
-	let i = 0;
-	while (i < x){
-		console.log("C is fun");
-		i++;
-	}
-}
-else{
-	console.log("Missing number of occurrences");
+if (x) {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,20 +2,18 @@
 
 const size = parseInt(process.argv[2]);
 
-if (size){
-	let i = 0;
-	let j = 0;
-	while (j < size){
-
-		while (i < size){
-			process.stdout.write('X');
-			i++;
-		}
-		j++;
-		i = 0;
-		console.log('');
-	}
-}
-else{
-	console.log("Missing size");
+if (size) {
+  let i = 0;
+  let j = 0;
+  while (j < size) {
+    while (i < size) {
+      process.stdout.write('X');
+      i++;
+    }
+    j++;
+    i = 0;
+    console.log('');
+  }
+} else {
+  console.log('Missing size');
 }
