048d0baae1913c297e0dc0debf65401ab162f2e5
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-08 02:04:44 -0800

    Task 0 - Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (size){
+	let i = 0;
+	let j = 0;
+	while (j < size){
+
+		while (i < size){
+			process.stdout.write('X');
+			i++;
+		}
+		j++;
+		i = 0;
+		console.log('');
+	}
+}
+else{
+	console.log("Missing size");
+}
