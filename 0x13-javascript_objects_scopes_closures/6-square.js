acad27ec572263d4932717942216d0708ac97423
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-09 03:58:57 -0800

    semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,21 +3,21 @@
 const oldSquare = require('./5-square');
 
 module.exports = class Square extends oldSquare {
-	constructor (size) {
-		super(size, size);
-		this.size = size;
-	}
-  	charPrint (c) {
-		if (c === undefined) {
-			super.print();
-		}
-		else {
-			for (let i = 0; i < this.size; i++) {
-				for (let j = 0; j < this.size; j++) {
-					process.stdout.write('C');
-				}
-				console.log('');
-			}
-		}
-	}
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          process.stdout.write('C');
+        }
+        console.log('');
+      }
+    }
+  }
 };
