7f193609d254d2e48de5adc2a723b28447d46797
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-09 03:45:48 -0800

    Task 0 -Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const oldSquare = require('./5-square');
+
+module.exports = class Square extends oldSquare {
+	constructor (size) {
+		super(size, size);
+		this.size = size;
+	}
+  	charPrint (c) {
+		if (c === undefined) {
+			super.print();
+		}
+		else {
+			for (let i = 0; i < this.size; i++) {
+				for (let j = 0; j < this.size; j++) {
+					process.stdout.write('C');
+				}
+				console.log('');
+			}
+		}
+	}
+};
